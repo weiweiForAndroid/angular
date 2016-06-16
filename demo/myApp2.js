e8cd5a5ecf925309c93f0f6976fc9b9a4437440c
wesson <superwesson@totiming.com>
2016-06-16 17:11:40 +0800

    Initial commit

---
+++ demo/myApp2.js
@@ -0,0 +1,11 @@
+/**
+ * Created by supershook on 2016/6/16.
+ */
+var app2 = angular.module("myApp2", [])
+// var app3 = angular.module("myApp3", [])
+app2.controller('myController2', ['$scope', function ($scope) {
+    $scope.name = "无双"
+}])
+app2.controller('myController3', ['$scope', function ($scope) {
+    $scope.name = "6666"
+}])
